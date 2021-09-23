150ff1825528c38908ee004974c063655f258c22
Ryagg <58631455+Ryagg@users.noreply.github.com>
2021-09-23 14:30:19 +0200

    feature: connect Flask to mongoDB

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 # needed because the file won't be found after deployment to heroku
 if os.path.exists("env.py"):
     import env
@@ -7,10 +11,22 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
+# setup an instance of PyMongo and add the app
+# this ensures that the Flask app can communicate with the Mongo database
+mongo = PyMongo(app)
+
+
+# placing these routes beneath each other results
+# in either URL directing the user to the same  page
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
