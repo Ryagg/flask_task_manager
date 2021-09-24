444184f5f97018be885a7b188879cea9b2366d8d
Ryagg <58631455+Ryagg@users.noreply.github.com>
2021-09-24 20:24:55 +0200

    feat: add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,14 @@ $(document).ready(function () {
     $(".sidenav").sidenav({ edge: "right" });
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        il8n: {
+            done: "Select",
+        },
+    });
 });
 
 /*
